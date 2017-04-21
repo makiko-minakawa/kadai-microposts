6270d0d0e5ace19999f1698492c6429abf276672
MakikoMinakawa <makimew@gmail.com>
2017-04-21 10:45:33 +0000

    modify favor-ctl

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -10,6 +10,7 @@ class FavoritesController < ApplicationController
   end
 
   def destroy
+    @micropost = Micropost.find(params[:micropost_id])
     @favorite = current_user.favorites.find_by!(micropost_id: @micropost.id)
     @favorite.destroy
     flash[:success] = 'お気に入りを解除しました。'
